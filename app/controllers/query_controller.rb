1b5bdd115e18a2cc1d1d3473484d673421cd27e2
Dylan Hanning <dylanhanning123@hotmail.com>
2014-10-18 19:37:32 +0100

    display basic estimates

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -5,6 +5,15 @@ class QueryController < ApplicationController
   end
 
   def submit
+    runner = Runner.new(age: params[:age], gender: params[:gender], fitness: params[:fitness], query_distance: params[:query_distance])
+    runner.save!
+
+    params['run'].each do |run|
+      run = Run.new(runner: runner, distance: run[1]['distance'].to_i, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
+      run.save!
+    end
+
+    @estimate = Runner.estimate(runner)
   end
 
 end
