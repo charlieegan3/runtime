50a02c94fdb68fa479eb7ec874cbf9cd1383bb99
Charlie Egan <charlieegan3@googlemail.com>
2014-10-19 19:02:06 +0100

    Tidy stats results page

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -8,23 +8,22 @@ class QueryController < ApplicationController
   end
 
   def submit
-
     if params[:units] == "k"
-      runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance])
-      runner.save!
+      @runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance])
+      @runner.save!
       params['run'].each do |run|
-        run = Run.new(runner: runner, distance: run[1]['distance'].to_i, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
+        run = Run.new(runner: @runner, distance: run[1]['distance'].to_i, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
         run.save!
       end
-      @estimate = Runner.estimate(runner)
+      @estimate = Runner.estimate(@runner)
     else
-      runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance].to_i * 1.609344)
-      runner.save!
+      @runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance].to_i * 1.609344)
+      @runner.save!
       params['run'].each do |run|
-        run = Run.new(runner: runner, distance: run[1]['distance'].to_i * 1.609344, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
+        run = Run.new(runner: @runner, distance: run[1]['distance'].to_i * 1.609344, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
         run.save!
       end
-      @estimate = Runner.estimate(runner)
+      @estimate = Runner.estimate(@runner)
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def formatted_time_from_seconds(time, colon = true)
+    string = Time.at(time).utc.strftime("%H:%M:%S")
+    if colon == false
+      string
+    else
+      string.split(':').join('<span class="colon">:</span>')
+    end
+  end
 end
