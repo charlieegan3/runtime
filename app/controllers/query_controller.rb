13a64e1a792f47d51db5233e27b0fc9d26ec6c45
Dylan Hanning <dylanhanning123@hotmail.com>
2014-10-19 15:22:10 +0100

    add distance units to form, and conversion in controller

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -8,15 +8,24 @@ class QueryController < ApplicationController
   end
 
   def submit
-    runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance])
-    runner.save!
 
-    params['run'].each do |run|
-      run = Run.new(runner: runner, distance: run[1]['distance'].to_i, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
-      run.save!
+    if params[:units] == "m"
+      runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance])
+      runner.save!
+      params['run'].each do |run|
+        run = Run.new(runner: runner, distance: run[1]['distance'].to_i, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
+        run.save!
+      end
+      @estimate = Runner.estimate(runner)
+    else
+      runner = Runner.new(age: params[:age], gender: params[:gender].downcase, fitness: params[:fitness], query_distance: params[:query_distance].to_i * 1.609344)
+      runner.save!
+      params['run'].each do |run|
+        run = Run.new(runner: runner, distance: run[1]['distance'].to_i * 1.609344, hours: run[1]['hours'].to_i, minutes: run[1]['minutes'].to_i, seconds: run[1]['seconds'].to_i)
+        run.save!
+      end
+      @estimate = Runner.estimate(runner)
     end
-
-    @estimate = Runner.estimate(runner)
   end
 
 end
