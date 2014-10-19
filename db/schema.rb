baba105d75f333af404afbd45960b6b8bbd1f9cf
Charlie Egan <charlieegan3@googlemail.com>
2014-10-19 13:17:53 +0100

    Distance model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018202446) do
+ActiveRecord::Schema.define(version: 20141019121652) do
+
+  create_table "distances", force: true do |t|
+    t.string   "identifier"
+    t.float    "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "runners", force: true do |t|
     t.integer  "age"
